9dd0aeb1a9c356eeecf264ab9564897b7e63a0d0
CESAR117B <csuarez@sodigsa.com>
2026-05-20 15:42:57 -0500

    firstoken update

--- microservicio/src/servicios/servicios.modules.ts
+++ microservicio/src/servicios/servicios.modules.ts
@@ -4,10 +4,11 @@ import { Servicios } from "./servicios";
 import { CardSaveController } from "./cardsave.controller";
 import { CardSaveService } from "./carsave.service";
 import { AuditoriaService } from "./auditoria.service";
+import { ApplicationService } from "./application.service";
 
 @Module({
     controllers:[ServicioController,CardSaveController],
-    providers:[Servicios,CardSaveService,AuditoriaService],
-    exports:[Servicios,CardSaveService,AuditoriaService]
+    providers:[Servicios,CardSaveService,AuditoriaService, ApplicationService],
+    exports:[Servicios,CardSaveService,AuditoriaService, ApplicationService]
 })
 export class ServiciosModule{}
