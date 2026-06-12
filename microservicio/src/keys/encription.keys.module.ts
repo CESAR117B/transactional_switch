5a7426110945d5341fef470f210c736108a72e10
CESAR117B <csuarez@sodigsa.com>
2026-06-12 14:39:08 -0500

    keys api class update

--- microservicio/src/keys/encription.keys.module.ts
+++ microservicio/src/keys/encription.keys.module.ts
@@ -1,6 +1,5 @@
 import { Module } from "@nestjs/common";
 import { EncriptionKeysController } from "./encription.keys.controller";
-import { ApplicationService } from "../servicios/application.service";
 
 
 @Module({
