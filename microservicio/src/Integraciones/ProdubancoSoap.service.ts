6cef79aa1c46675006190f004e7e1db0c0102c1f
CESAR117B <csuarez@sodigsa.com>
2026-09-14 17:08:12 -0500

    autenbtuicacion produbanco actualizada

--- microservicio/src/Integraciones/ProdubancoSoap.service.ts
+++ microservicio/src/Integraciones/ProdubancoSoap.service.ts
@@ -66,7 +66,7 @@ export class ProdubancoSoapService {
       const client = await this.getClient();
 
       const payload = {
-        strComputador: '127.0.0.1', 
+        strComputador: '', 
         strUsuario: config.usuario,
         strXmlInput: xmlContenido,
         Empresa: config.empresa,
@@ -132,7 +132,7 @@ export class ProdubancoSoapService {
     const payload = {
       strXmlInput: tramaEncriptada,
       strUsuario: config.usuario,
-      strComputador: '127.0.0.1', // Usar la IP autorizada en Produbanco
+      strComputador: '', // Usar la IP autorizada en Produbanco
       Empresa: config.empresa,
     };
 
